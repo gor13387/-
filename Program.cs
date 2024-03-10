df57baa83dfacdf8546a09c0d4452220f9788445
ANDREY <gor13387@gmail.com>
2024-03-10 11:15:52 +0300

    вывод списка команд и заполнение массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выводим список команд для пользователя
+        Commands();
+
+        // Инициализируем массив и запрашиваем команду у пользователя
+        string[] array = new string[] { };
+        string fromUser = ReadInput("Введите команду: ");
+
+        // Обработка команды пользователя и заполнение массива соответствующими данными
+        switch (fromUser)
+        {
+            case "1":
+                array = new string[] { "Hello", "2", "world", ":-)" };
+                break;
+            case "2":
+                array = new string[] { "1234", "1567", "-2", "computer science" };
+                break;
+            case "3":
+                array = new string[] { "Russia", "Denmark", "Kazan" };
+                break;
+            default:
+                Console.WriteLine($"{fromUser} - Такой команды нет");
+                break;
+        }
+
+        
+
